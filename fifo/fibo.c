301d1d8bb15d1fa4fc33f3c941f9044d93b160c0
rarul <rarul@rarul.com>
2020-07-25 17:02:29 +0900

    add fibonacci bench

---
+++ fifo/fibo.c
@@ -0,0 +1,41 @@
+#include  <stdio.h>
+#include  <time.h>
+
+static void time_calc_sta(struct timespec *ts){
+	clock_gettime(CLOCK_MONOTONIC, ts);
+}
+static void time_calc_end(struct timespec *sta_ts, const char *msg){
+	struct timespec end_ts;
+	clock_gettime(CLOCK_MONOTONIC, &end_ts);
+}
+static int val_fibo(int n) {
+	if (n < 2) {
+		return n;
+	} else {
+		return val_fibo(n-1) + val_fibo(n-2);
+	}
+}
+
+static void do_fibo(int n) {
+	struct timespec sta_time, end_time;
+
+	clock_gettime(CLOCK_MONOTONIC, &sta_time);
+	int result_fibo = val_fibo(n);
+	clock_gettime(CLOCK_MONOTONIC, &end_time);
+
+	if (sta_time.tv_nsec > end_time.tv_nsec) {
+		end_time.tv_nsec += 1000*1000*1000;
+		end_time.tv_sec -= 1;
+	}
+	long long diff_time_msec = 1000*(end_time.tv_sec - sta_time.tv_sec) + (end_time.tv_nsec - sta_time.tv_nsec) / (1000*1000);
+	printf("[%lld.%03lld] fibo(%d) = %d\n", diff_time_msec / 1000, diff_time_msec % 1000,
+		   n, result_fibo);
+
+}
+
+int main(int argc, char *argv[]){
+	do_fibo(38);
+	do_fibo(39);
+	do_fibo(40);
+	return 0;
+}
